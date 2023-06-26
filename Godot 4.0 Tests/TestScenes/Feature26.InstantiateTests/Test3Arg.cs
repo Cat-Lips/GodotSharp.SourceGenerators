4d773d9831ab1262f7e438d4fab09c307125bf50
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2023-06-26 16:42:53 +1000

    #26 Adding explicit invalid chars list to ensure valid filename on all platforms

--- Godot 4.0 Tests/TestScenes/Feature26.InstantiateTests/Test3Arg.cs
+++ Godot 4.0 Tests/TestScenes/Feature26.InstantiateTests/Test3Arg.cs
@@ -6,12 +6,12 @@ namespace GodotTests.TestScenes
     public partial class Test3Arg : Control
     {
         public int A { get; private set; }
-        public int B { get; private set; }
-        public int C { get; private set; }
+        public int? B { get; private set; }
+        public int? C { get; private set; }
         [Export] public int X { get; set; }
 
         [OnInstantiate]
-        private void Initialise(int a, int b, int c)
+        private void Initialise(int a, int? b, int? c = null)
         {
             A = a;
             B = b;
