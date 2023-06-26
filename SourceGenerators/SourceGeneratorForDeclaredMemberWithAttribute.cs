acabb99344c55b9e0187cc84d6941db75f435564
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2023-06-26 16:42:53 +1000

    #26/#33 Adding explicit invalid chars list to ensure valid filename on all platforms

--- SourceGenerators/SourceGeneratorForDeclaredMemberWithAttribute.cs
+++ SourceGenerators/SourceGeneratorForDeclaredMemberWithAttribute.cs
@@ -102,9 +102,25 @@ namespace GodotSharp.SourceGenerators
         }
 
         protected virtual string GenerateFilename(ISymbol symbol)
-            => $"{string.Join("_", $"{symbol}".Split(Path.GetInvalidFileNameChars()))}.g.cs";
+        {
+            var gn = $"{Format(symbol)}.g.cs";
+            Log.Debug($"Generated Filename: {gn}\n");
+            return gn;
+
+            static string Format(ISymbol symbol)
+                => string.Join("_", $"{symbol}".Split(InvalidFileNameChars));
+        }
 
         protected virtual SyntaxNode Node(TDeclarationSyntax node)
             => node;
+
+        private static readonly char[] InvalidFileNameChars = new char[]
+        {
+            '\"', '<', '>', '|', '\0',
+            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+            (char)31, ':', '*', '?', '\\', '/'
+        };
     }
 }
