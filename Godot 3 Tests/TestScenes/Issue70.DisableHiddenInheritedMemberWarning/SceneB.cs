18725d1b6c356c200811cc98251a84184731b09d
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2024-09-30 18:39:34 +1000

    #70 Adding failing tests

---
+++ Godot 3 Tests/TestScenes/Issue70.DisableHiddenInheritedMemberWarning/SceneB.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace GodotTests.TestScenes
+{
+    public partial class SceneB : SceneA
+    {
+        [OnInstantiate] // Success if compiles
+        private void Init() { }
+    }
+}
---
+++ Godot 4 Tests/TestScenes/Issue70.DisableHiddenInheritedMemberWarning/SceneA.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace GodotTests.TestScenes
+{
+    public partial class SceneA : Node
+    {
+        [OnInstantiate] // Success if compiles
+        private void Init() { }
+    }
+}
