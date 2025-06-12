fede96c942f93205cc14291d1f2779400ca1ef2f
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-06-13 00:35:49 +0100

    Use Godot preproc defs to identify Godot version in templates (#104)

--- SourceGenerators/OnImportExtensions/Resources.cs
+++ SourceGenerators/OnImportExtensions/Resources.cs
@@ -9,7 +9,7 @@ internal static class Resources
 
     public static readonly string HintAttribute = @"
 #if GODOT
-#if NET6_0_OR_GREATER // Godot 4 only
+#if GODOT4_OR_GREATER
 using System;
 
 namespace Godot
@@ -32,7 +32,7 @@ namespace Godot
 
     public static readonly string OnImportEditorPlugin = @"
 #if GODOT && TOOLS
-#if NET6_0_OR_GREATER // Godot 4 only
+#if GODOT4_OR_GREATER
 using Godot.Collections;
 
 namespace Godot
