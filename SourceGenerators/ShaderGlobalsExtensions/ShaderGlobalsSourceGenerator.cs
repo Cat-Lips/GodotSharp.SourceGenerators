9b2d2c67165a1e0fd1eb73e173267f65c6b67e39
Stefan <29021710+Saalvage@users.noreply.github.com>
2025-11-24 06:36:04 +0100

    Statically typed shader global wrapper (#164)

---
+++ SourceGenerators/ShaderGlobalsExtensions/ShaderGlobalsSourceGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Scriban;
+
+namespace GodotSharp.SourceGenerators.ShaderGlobalsExtensions;
+
+[Generator]
+internal class ShaderGlobalsSourceGenerator : SourceGeneratorForDeclaredTypeWithAttribute<Godot.ShaderGlobalsAttribute>
+{
+    private static Template ShaderGlobalsTemplate => field ??= Template.Parse(Resources.ShaderGlobalsTemplate);
+
+    protected override (string GeneratedCode, DiagnosticDetail Error) GenerateCode(Compilation compilation, SyntaxNode node, INamedTypeSymbol symbol, AttributeData attribute, AnalyzerConfigOptions options)
+    {
+        var data = ReconstructAttribute();
+        var model = new ShaderGlobalsDataModel(symbol, data.ClassPath, options.TryGetGodotProjectDir());
+        Log.Debug($"--- MODEL ---\n{model}\n");
+
+        var output = ShaderGlobalsTemplate.Render(model, member => member.Name);
+        Log.Debug($"--- OUTPUT ---\n{output}<END>\n");
+
+        return (output, null);
+
+        Godot.ShaderGlobalsAttribute ReconstructAttribute() => new(
+            (string)attribute.ConstructorArguments[0].Value);
+    }
+}
