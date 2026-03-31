9f7abf2a907a489630c56655d406d7e5a022b7c3
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2026-03-31 19:37:24 +1100

    Adding fix

--- Godot 4 Tests/ManualTests/Issue180.NotifyEditorDefaults/EditorDefaultTest.cs
+++ Godot 4 Tests/ManualTests/Issue180.NotifyEditorDefaults/EditorDefaultTest.cs
@@ -4,16 +4,16 @@ using Godot;
 public partial class EditorDefaultTest : Node
 {
     [Export, Notify] public partial int PartialNoDefault { get; set; }
-    [Export, Notify] public partial int PartialCtorDefault { get; set; }
-    //[Export, Notify] public partial int PartialInlineDefault { get; set; } = 7;
+    //[Export, Notify] public partial int PartialCtorDefault { get; set; }
+    [Export, Notify] public partial int PartialInlineDefault { get; set; } = 7;
 
     [Export, Notify] public int NonPartialNoDefault { get => _nonPartialNoDefault.Get(); set => _nonPartialNoDefault.Set(value); }
-    [Export, Notify] public int NonPartialCtorDefault { get => _nonPartialCtorDefault.Get(); set => _nonPartialCtorDefault.Set(value); }
-    //[Export, Notify] public int NonPartialInlineDefault { get => _nonPartialCtorDefault.Get(); set => _nonPartialCtorDefault.Set(value); } = 7;
+    //[Export, Notify] public int NonPartialCtorDefault { get => _nonPartialCtorDefault.Get(); set => _nonPartialCtorDefault.Set(value); }
+    [Export, Notify] public int NonPartialInlineDefault { get => field = _nonPartialInlineDefault.Get(); set => _nonPartialInlineDefault.Set(field = value); } = 7;
 
     public EditorDefaultTest()
     {
-        PartialCtorDefault = 7;
-        NonPartialCtorDefault = 7;
+        //PartialCtorDefault = 7;
+        //NonPartialCtorDefault = 7;
     }
 }
