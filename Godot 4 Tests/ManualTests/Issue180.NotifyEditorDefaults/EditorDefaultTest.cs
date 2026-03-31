10f15e8085cc4bc9eac1a5d6e0416a5c41396594
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2026-03-31 19:19:42 +1100

    Adding manual test

---
+++ Godot 4 Tests/ManualTests/Issue180.NotifyEditorDefaults/EditorDefaultTest.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+[Tool]
+public partial class EditorDefaultTest : Node
+{
+    [Export, Notify] public partial int PartialNoDefault { get; set; }
+    [Export, Notify] public partial int PartialCtorDefault { get; set; }
+    //[Export, Notify] public partial int PartialInlineDefault { get; set; } = 7;
+
+    [Export, Notify] public int NonPartialNoDefault { get => _nonPartialNoDefault.Get(); set => _nonPartialNoDefault.Set(value); }
+    [Export, Notify] public int NonPartialCtorDefault { get => _nonPartialCtorDefault.Get(); set => _nonPartialCtorDefault.Set(value); }
+    //[Export, Notify] public int NonPartialInlineDefault { get => _nonPartialCtorDefault.Get(); set => _nonPartialCtorDefault.Set(value); } = 7;
+
+    public EditorDefaultTest()
+    {
+        PartialCtorDefault = 7;
+        NonPartialCtorDefault = 7;
+    }
+}
